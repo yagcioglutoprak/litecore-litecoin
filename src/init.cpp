5d6b8812d9d460e746076a5f6f39bfaedae2a940
yagcioglutoprak <40343443+yagcioglutoprak@users.noreply.github.com>
2019-07-04 08:16:38 +0300

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -1481,14 +1481,7 @@ bool AppInit2(boost::thread_group& threadGroup, CScheduler& scheduler)
 
     // if pruning, unset the service bit and perform the initial blockstore prune
     // after any wallet rescanning has taken place.
-    if (fPruneMode) {
-        LogPrintf("Unsetting NODE_NETWORK on prune mode\n");
-        nLocalServices = ServiceFlags(nLocalServices & ~NODE_NETWORK);
-        if (!fReindex) {
-            uiInterface.InitMessage(_("Pruning blockstore..."));
-            PruneAndFlush();
-        }
-    }
+    doNotBroadcastBlocks = GetBoolArg("-dontbroadcastblocks", false);
 
     if (Params().GetConsensus().vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout != 0) {
         // Only advertize witness capabilities if they have a reasonable start time.
